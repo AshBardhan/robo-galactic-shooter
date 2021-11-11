dbe30bd2e8ba164dd6954593357481df3b844656
AshBardhan <ashbardhan@gmail.com>
2021-11-11 09:33:03 +0530

    Remove 'grunt-zip' dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,6 @@ module.exports = function (grunt) {
 				dest: 'dist/'
 			}
 		},
-		zip: {
-			'using-cwd': {
-				cwd: 'dist/',
-				src: ['dist/**'],
-				dest: 'robo-galactic-shooter.zip'
-			}
-		},
 		watch: {
 			'src': {
 				files: ['src/**'],
@@ -40,9 +33,8 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify-es');
-	grunt.loadNpmTasks('grunt-zip');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('build', ['concat', 'copy', 'uglify:js', 'zip']);
+	grunt.registerTask('build', ['concat', 'copy', 'uglify:js']);
 	grunt.registerTask('test', ['concat', 'watch']);
 };
