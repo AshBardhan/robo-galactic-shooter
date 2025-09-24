ef9d475856d00a326a18d0af61c223094eb48797
AshBardhan <ashbardhan@gmail.com>
2025-09-24 21:46:29 +0530

    Game running after successful production 'build' process

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src",
+  root: ".",
   build: {
-    outDir: "../dist",
+    outDir: "dist",
+    emptyOutDir: true
   },
 });
