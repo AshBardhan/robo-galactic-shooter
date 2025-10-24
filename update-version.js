e8f8e28791fdf572ebda328c6544bb68a21aa420
AshBardhan <ashbardhan@gmail.com>
2025-10-24 06:33:35 +0530

    Convert 'update-version' file in ESM format

--- update-version.js
+++ update-version.js
@@ -1,18 +1,20 @@
-const fs = require("fs");
-const path = require("path");
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Read the version from package.json
-const packageJson = require("./package.json");
+const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 const newVersion = packageJson.version;
 
 // Define the paths for files to update
-const filesToUpdate = [
-  path.join(__dirname, "README.md"),
-];
+const filesToUpdate = [path.join(__dirname, 'README.md')];
 
 // Function to update the version in JSON files like manifest.json
 function updateJsonVersion(filePath, version) {
-  const file = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  const file = JSON.parse(fs.readFileSync(filePath, 'utf8'));
   file.version = version;
   fs.writeFileSync(filePath, JSON.stringify(file, null, 2));
   console.log(`Updated version in ${filePath} to ${version}`);
@@ -20,7 +22,7 @@ function updateJsonVersion(filePath, version) {
 
 // Function to update markdown files like README.md
 function updateMarkdownVersion(filePath, version) {
-  let fileContent = fs.readFileSync(filePath, "utf8");
+  let fileContent = fs.readFileSync(filePath, 'utf8');
   // This regex will look for 'Current version: vx.x.x' format
   const versionRegex = /\*\*Latest Release:\*\*\s?v\d+\.\d+\.\d+/;
   const newVersionLine = `**Latest Release:** v${newVersion}`;
@@ -32,7 +34,7 @@ function updateMarkdownVersion(filePath, version) {
 
 // Function to update version in HTML files
 function updateHtmlVersion(filePath, version) {
-  let fileContent = fs.readFileSync(filePath, "utf8");
+  let fileContent = fs.readFileSync(filePath, 'utf8');
   // Replace the version number in the <p> tag with the version in the HTML file
   const versionRegex = /(Version:\s*)(\d+\.\d+\.\d+)/;
   fileContent = fileContent.replace(versionRegex, `$1${version}`);
@@ -42,11 +44,11 @@ function updateHtmlVersion(filePath, version) {
 
 // Loop through all the files and update their version
 filesToUpdate.forEach((filePath) => {
-  if (filePath.endsWith(".json")) {
+  if (filePath.endsWith('.json')) {
     updateJsonVersion(filePath, newVersion);
-  } else if (filePath.endsWith(".md")) {
+  } else if (filePath.endsWith('.md')) {
     updateMarkdownVersion(filePath, newVersion);
-  } else if (filePath.endsWith(".html")) {
+  } else if (filePath.endsWith('.html')) {
     updateHtmlVersion(filePath, newVersion);
   }
 });
